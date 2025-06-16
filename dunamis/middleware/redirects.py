69e794b15f80c6758920d01d481b69450433d36d
NephygTdkingz <tdkingzict@gmail.com>
2025-06-16 14:39:20 +0100

    fixed th middleware to accept non secure redirect

--- dunamis/middleware/redirects.py
+++ dunamis/middleware/redirects.py
@@ -6,12 +6,12 @@ class ForceWWWAndHTTPSMiddleware:
 
     def __call__(self, request):
         host = request.get_host()
-        is_secure = request.is_secure()
         path = request.get_full_path()
 
-        # Redirect if not HTTPS or not www
-        if not is_secure or not host.startswith('www.'):
+        # If not HTTPS or host does not start with www., redirect to https://www.dunamislawfirm.com
+        if not request.is_secure() or not host.startswith('www.'):
             new_url = f'https://www.dunamislawfirm.com{path}'
             return HttpResponsePermanentRedirect(new_url)
 
         return self.get_response(request)
+
